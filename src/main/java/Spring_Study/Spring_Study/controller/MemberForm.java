af60f42c226857ebd5b33d301f1e1d26ebcd044b
PARK9898 <45031880+PARK9898@users.noreply.github.com>
2023-03-01 18:04:43 +0900

    Create MemberForm

---
+++ src/main/java/Spring_Study/Spring_Study/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package Spring_Study.Spring_Study.controller;
+
+public class MemberForm {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
