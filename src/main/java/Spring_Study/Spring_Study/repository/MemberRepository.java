4e18227296a52cffb7d856ba38f20d88a19fd751
PARK9898 <45031880+PARK9898@users.noreply.github.com>
2023-03-01 16:48:49 +0900

    Refactoring MemberRepository

--- src/main/java/Spring_Study/Spring_Study/repository/MemberRepository.java
+++ src/main/java/Spring_Study/Spring_Study/repository/MemberRepository.java
@@ -3,12 +3,15 @@ package Spring_Study.Spring_Study.repository;
 import java.util.List;
 import java.util.Optional;
 
-import Spring_Study.Spring_Study.domain.Member;
+import org.springframework.stereotype.Repository;
 
+import Spring_Study.Spring_Study.domain.Member;
+@Repository
 public interface MemberRepository {
 	Member save(Member member);
-	Optional<Member> findById(Long id);
+	Optional<Member> findById(Long id); // optional 을 null 을 처리하기 위한 기법임
 	Optional<Member> findByName(String name);
 	List<Member> findAll();
 
+	
 }
